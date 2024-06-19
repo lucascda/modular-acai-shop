ee7f2d97051e49b4d718e4779caebf203df566a3
Lucas César <43433070+lucascda@users.noreply.github.com>
2024-06-19 20:28:40 -0300

    UserRepository interface

---
+++ internal/auth/domain/repository/user_repository.go
@@ -0,0 +1,11 @@
+package repository
+
+import (
+	"context"
+	"modular-acai-shop/internal/auth/domain/entity"
+)
+
+type UserRepository interface {
+	GetUserByEmail(ctx context.Context, email string) (*entity.User, error)
+	GetUserById(ctx context.Context, id string) (*entity.User, error)
+}
