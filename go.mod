6508a33cccfc5af9a1594b4583c8825629e28b1c
Lucas César <43433070+lucascda@users.noreply.github.com>
2024-06-19 20:24:15 -0300

    user entity

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module modular-acai-shop
 
 go 1.22.1
+
+require github.com/google/uuid v1.6.0 // indirect
---
+++ internal/auth/domain/entity/user.go
@@ -0,0 +1,31 @@
+package entity
+
+import "github.com/google/uuid"
+
+type User struct {
+	id       string
+	name     string
+	email    string
+	password string
+}
+
+func NewUserEntity(id, name, email, password string) *User {
+	return &User{
+		id:       uuid.New().String(),
+		name:     name,
+		email:    email,
+		password: password,
+	}
+}
+
+func (u *User) ID() string {
+	return u.id
+}
+
+func (u *User) Name() string {
+	return u.name
+}
+
+func (u *User) Email() string {
+	return u.email
+}
